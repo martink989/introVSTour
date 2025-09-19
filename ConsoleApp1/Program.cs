fbf50051b15afbdcd930c23e1ebc1b635e0f6655
opilane <opilane@HP-CZC321706M>
2025-09-19 15:03:28 +0300

    ToaKalkulaator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -55,44 +55,172 @@ string sõnum = "Teie vanus on: ";
 //Console.WriteLine($"Tehte tulemus: {korrutav}");
 //Console.WriteLine("Tehte tulemus: "+korrutav);
 
-int arv1 = 0;
-int arv2 = 0;
-Console.WriteLine("Tere, palun sisesta esimene arv: ");
-arv1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Palun sisesta teine arv: ");
-arv2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / *");
-string tehtetüüp = Console.ReadLine();
-int tehe = 0;
+//int arv1 = 0;
+//int arv2 = 0;
+//Console.WriteLine("Tere, palun sisesta esimene arv: ");
+//arv1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Palun sisesta teine arv: ");
+//arv2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Missugust tehet soovid teha? Kirjuta kas: + - / *");
+//string tehtetüüp = Console.ReadLine();
+//int tehe = 0;
 
-if (tehtetüüp == "+")
+//if (tehtetüüp == "+")
+//{
+//    tehe = arv1 + arv2;
+//}
+
+//if (tehtetüüp == "-")
+//{
+//    tehe = arv1 - arv2;
+//}
+
+//if (tehtetüüp == "/")
+//{
+//    tehe = arv1 / arv2;
+//}
+
+//if (tehtetüüp == "*")
+//{
+//    tehe = arv1 * arv2;
+//}
+
+//if (tehtetüüp == "^")
+//{
+//    tehe = (int)Math.Pow(arv1, arv2);
+//}
+
+//Console.WriteLine($"Tehte tulemus: {tehe}");
+//string lemmikpuuvili = "";
+//string valik = "";
+//string lemmikjuurvili;
+//string lemmikköögivili;
+//Console.WriteLine("Kas sulle meeldivad puuviljad, köögiviljad või juurviljad ?");
+//valik = Console.ReadLine().ToString();
+
+
+//if (valik == "puuviljad")
+//{
+//    Console.WriteLine("Milline puuvili on sinu lemmik?");
+//    lemmikpuuvili = Console.ReadLine();
+
+//    if (lemmikpuuvili == "maasikas")
+//    {
+//        Console.WriteLine("Maasikas ei ole puuvili");
+//    }
+//    else if (lemmikpuuvili == "pirn")
+//    {
+//        Console.WriteLine("Kas hapu pirn või magus pirn, mõlemad on head.");
+//    }
+//    else if (lemmikpuuvili == "kartul")
+//    {
+//        Console.WriteLine("See ei kasva isegi mitte puu otsas");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ei tunne seda puuvilja");
+//    }
+//}
+//else if (valik == "juurviljad")
+//{
+//    Console.WriteLine("Milline juurvili on sinu lemmik?");
+//    lemmikjuurvili = Console.ReadLine();
+
+//    if (lemmikjuurvili == "õun")
+//    {
+//        Console.WriteLine("õun ei kasva juurtega");
+//    }
+//    else if (lemmikjuurvili == "sibul")
+//    {
+//        Console.WriteLine("Vinge");
+//    }
+//    else if (lemmikjuurvili == "banaan")
+//    {
+//        Console.WriteLine("Banaan kasvab puu otsas");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ei tunne seda juurvilja");
+//    }
+//}
+//else if (valik == "köögiviljad")
+//{
+//    Console.WriteLine("Milline köögivili on sinu lemmik?");
+//    lemmikköögivili = Console.ReadLine();
+
+//    if (lemmikköögivili == "pelmeen")
+//    {
+//        Console.WriteLine("Ei ole köögivili");
+//    }
+//    else if (lemmikköögivili == "kartul")
+//    {
+//        Console.WriteLine("Kas magus kartul või tavaline kartul ?");
+
+//    }
+//    else if (lemmikköögivili == "maasikas")
+//    {
+//        Console.WriteLine("maasikas ei ole köögivili");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ei tunne seda köögivilja");
+//    }
+//}
+
+//else
+//{
+//    Console.WriteLine("Seda valikut ei ole");
+//}
+
+int a_kulg = 0;
+int b_kulg = 0;
+int c_kulg = 0;
+int läbimõõt = 0;
+string valik = "";
+Console.WriteLine("Tere Kas teie tuba on kolmnurga, ristküliku või ringi kujuline?");
+valik = Console.ReadLine().ToString();
+
+if (valik == "kolmnurk") 
 {
-    tehe = arv1 + arv2;
+    Console.WriteLine("Palun sisesta oma toa esimene külg");
+    a_kulg = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Sisesta oma toa teine külg");
+    b_kulg = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Sisesta oma toa kolmas külg");
+    c_kulg = int.Parse(Console.ReadLine());
+
+    int kujund = (a_kulg + b_kulg + c_kulg) / 2;
+    Console.WriteLine($"Toa pindala on: {kujund}");
 }
 
-if (tehtetüüp == "-")
+
+
+else if (valik == "ristkülik")
 {
-    tehe = arv1 - arv2;
+    Console.WriteLine("Palun sisesta toa esimene külg");
+    a_kulg = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Sisesta oma toa teine külg");
+    b_kulg = int.Parse(Console.ReadLine());
+
+    int kujund2 = a_kulg * b_kulg;
+    Console.WriteLine($"Toa pindala on: {kujund2}");
 }
 
-if (tehtetüüp == "/")
+else if (valik == "ring")
 {
-    tehe = arv1 / arv2;
-}
+    Console.WriteLine("Palun sisesta oma toa läbimõõt");
+    läbimõõt = int.Parse(Console.ReadLine());
 
-if (tehtetüüp == "*")
+    int kujund3 = Math.PI *  Math.Pow(läbimõõt/2);
+    Console.WriteLine($"Toa pindala on: {kujund3}");
+}
+else
 {
-    tehe = arv1 * arv2;
+    Console.WriteLine("Ei tunne seda kujundit");
 }
-
-if (tehtetüüp == "^")
-{
-    tehe = (int)Math.Pow(arv1, arv2);
-}
-
-Console.WriteLine($"Tehte tulemus: {tehe}");
-
-
 
 // + liitmine
 // - lahutame
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b06428a88d729eecac6fa7dd4ea28cc4c3c8352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b317ac38c71e371142311597ae9e57d2207ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
